963ab3fe714253b1387400493fb35f1644e5ff25
Synnoid <synnbiot@gmail.com>
2012-08-30 15:23:17 +0300

    5.0.4 - Prerelease [Untested]

--- Rotations/warlock_destro.lua
+++ Rotations/warlock_destro.lua
@@ -1,49 +1,72 @@
-function warlock_destro(self)
-	local mana = UnitMana("player")/UnitManaMax("player");
-	local shards = UnitPower("player",7);
-	local spell = nil;
+function warlock_destruction(self)
 
-	local isf_duration = jps.buffDuration("improved soul fire");
-	local bod_duration = jps.debuffDuration("bane of doom");
-	local cpn_duration = jps.debuffDuration("corruption");
-	local isb_duration = jps.debuffDuration("shadow and flame");
-	local imo_duration = jps.debuffDuration("immolate");
+	local mana = UnitMana("player")/UnitManaMax("player")
+	local hp = UnitHealth("player")/UnitHealthMax("player")
+	local targetHp = UnitHealth("target")/UnitHealthMax("target")
+	local mousoverHp = UnitHealth("mouseover")/UnitHealthMax("mouseover")
 
-	if cd("demon soul") == 0 then 
-		spell = "demon soul";
-	elseif cd("lifeblood") == 0 then
-		spell = "lifeblood";
-	elseif not ud("target","curse of the elements") then
-		spell = "curse of the elements";
-	elseif cd("soulburn") == 0 and shards > 0 then
-		spell = "soulburn";
-	elseif ub("player","soulburn") or ub("player","empowered imp") then
-		spell = "soul fire";
-	elseif bod_duration < 2 then
-		spell = "bane of doom";
-	elseif cpn_duration < 2 then
-		spell = "corruption";
-	elseif GetUnitSpeed("player") > 0 then
-		spell = "fel flame";
-	elseif isf_duration < 3 and jps.LastCast ~= "soul fire" then
-		spell = "soul fire";
-	elseif not ud("target","shadow and flame") and jps.LastCast ~= "shadow bolt" then
-		spell = "shadow bolt";
-	elseif IsSpellInRange("shadowflame","target") == 1 and cd("shadowflame") == 0 then
-		spell = "shadowflame";
-	elseif imo_duration < 2 and jps.LastCast ~= "immolate" then
-		spell = "immolate";
-	elseif cd("conflagrate") == 0 and ud("target","immolate") then
-		spell = "conflagrate";
-	elseif cd("chaos bolt") == 0 then
-		spell = "chaos bolt";
-	elseif mana < 0.3 then
-		spell = "life tap";
-	else
-		spell = "incinerate";
+	local embers = UnitPower("player",14)
+	local hasEmbers = false
+
+	if embers > 0 then 
+		hasEmbers = true
 	end
+	
 
-	return spell;
+	if jps.MultiTarget then
+		--AOE
+		--Lets burn this shift! 
 
-end		
+		--TODO: Count our engaged targets
 
+		--Burn them down!
+		if jps.checkTimer("RoF") == nil or jps.checkTimer("RoF") == 0 then
+			createTimer("RoF", 6)
+			jps.groundClick()
+			return "Rain of Fire"
+		end
+
+		--We just FaB'ed lets figure out if we should aoe conflag or aoe immolate or aoe incinerate.
+		if jps.buff("Fire and Brimstone") then
+			if jps.debuffDuration("immolate", "mouseover") == 0 then
+				return "Immolate"
+			else
+				if cd("conflagrate") == 0 then
+					return "Conflagrate"
+				else
+					return "Incinerate"
+				end
+			end
+		end
+
+		--Do we have enough embers for an Fire and Brimstone
+		if embers >= 2 then
+				--Fire And Brimstone this shizzle
+				return "Fire and Brimstone"
+			end
+		end
+
+		--We blew all our fancy shizzle just dot em the old school way
+		--TODO: Research if building embers for an aoe immolate results in more damage.
+		if 	jps.debuffDuration("immolate", "mouseover")==0
+			and UnitAffectingCombat("mouseover") 
+			and mouseoverHp > 20
+			and UnitThreatSituation("player","mousover")<2 then
+			jps.Target = "mouseover"
+			return "Immolate"
+		end
+	else
+		--SINGLE TARGET
+
+		--Lets check if we need to bring the magic vulnerability and if there's a point wasting a gcd on one.
+		if (jps.notmyDebuffDuration("curse of elements") == 0 and jps.notmyDebuffDuration("master poisoner") == 0)
+			and ( ( targetHp > 20 and UnitHealth("target") > 1000000 ) or ( UnitHealth("target") > 1000000 ) ) then --Arbitrary hp numbers, will fine tune this later.
+			return "Curse of Elements"
+		end
+
+		if hasEmbers and UnitHealth("target") > 100000 --currently i'm hitting arround 100k minimum with this one, will update to scale with level 90 dps later.
+			return "Chaos Bolt"
+		end
+
+	end
+end
